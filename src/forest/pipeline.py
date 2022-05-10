061bd261d00f5ae0295984630a4ba9dc2d80ba4d
devbrest <75991746+devbrest@users.noreply.github.com>
2022-05-11 00:39:29 +0300

    Update pipeline.py

--- src/forest/pipeline.py
+++ src/forest/pipeline.py
@@ -1,8 +1,9 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SequentialFeatureSelector, SelectFromModel
+from sklearn.tree import DecisionTreeClassifier
 
 def create_pipeline(
     use_scaler: bool, max_iter: int, logreg_C: float, random_state: int
@@ -18,4 +19,46 @@ def create_pipeline(
             ),
         )
     )
+    return Pipeline(steps=pipeline_steps)
+def create_pipeline_both_model(
+    use_scaler: bool, max_iter: int, logreg_C: float, random_state: int, f_eng: str, cl: str, max_depth: int
+) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+    selection_model = RandomForestClassifier(max_depth=13)
+    if f_eng == 'SequentialFeatureSelector':
+        pipeline_steps.append(
+            (
+            "feature_selection",
+            SequentialFeatureSelector(estimator=selection_model)
+            )    
+        )
+    elif f_eng == 'SelectFromModel':
+        pipeline_steps.append(
+            (
+            "feature_selection",
+            SelectFromModel(estimator=selection_model)
+            )    
+        )    
+   
+    if cl == 'LogisticRegression':
+        
+        pipeline_steps.append(
+            (
+                "classifier",
+                LogisticRegression(
+                    random_state=random_state, max_iter=max_iter, C=logreg_C
+                ),
+            )
+        )
+    elif cl == 'DecisionTreeClassifier':
+        pipeline_steps.append(
+            (
+                "classifier",
+                DecisionTreeClassifier(
+                    random_state=random_state, max_depth=max_depth
+                ),
+            )
+        )
     return Pipeline(steps=pipeline_steps)
